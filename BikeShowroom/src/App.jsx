c871ffbb1ce68924649bb77afa2124ac752321dc
pratappatil16 <ppratappatil775@gmail.com>
2024-04-01 20:32:37 +0530

    Created folder for better understanding and navbar

--- BikeShowroom/src/App.jsx
+++ BikeShowroom/src/App.jsx
@@ -2,21 +2,16 @@ import { useState } from 'react'
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import './App.css'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App' >
-      <Canvas >
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 3, 5]} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
-        </mesh>
-        <OrbitControls></OrbitControls>
-      </Canvas>
+    <Navbar></Navbar>
+    <Home></Home>
     </div>
   )
 }
