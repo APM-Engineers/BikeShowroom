82f0855949461512ba72057fa87fe61af5f2ffcb
Mandeep Narotta <narotta@gmail.com>
2024-05-02 15:31:58 +0530

    model

--- BikeShowroom/src/pages/Build.jsx
+++ BikeShowroom/src/pages/Build.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Model } from "../components/Model";
 
@@ -6,15 +6,11 @@ export default function Build() {
     return (
         <div className="w-screen h-screen">
             <Canvas>
-                {/* Ambient light */}
-                <ambientLight intensity={0.5} />
-
-                {/* Directional light */}
-                <directionalLight color="white" intensity={0.5} position={[0, 0, 5]} />
-
-                    <Model scale={0.4} />
-                    <OrbitControls></OrbitControls>
-               
+                {/* <ambientLight intensity={2} /> */}
+                <Environment preset="city" background={true} />
+                <directionalLight color="white" intensity={1} position={[0, 0, 5]} />
+                <Model />
+                <OrbitControls></OrbitControls>
             </Canvas>
         </div>
     );
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BikeShowroom
 fork done
+
+
+https://sketchfab.com/3d-models/honda-shadow-rs-gold-2010-by-alexka-21c555480ce24ad6931d1c04b6a8a7c1
